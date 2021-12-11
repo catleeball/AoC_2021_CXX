b794a4d70177ac534c6307c7a45db9fe04ad0ead
Cat Lee Ball 🎶🎷🐀 <cat@catball.dev>
2021-12-11 14:13:56 -0800

    Day one, part one complete

---
+++ day_one.h
@@ -0,0 +1,15 @@
+//
+// Created by cat on 12/11/21.
+//
+
+#ifndef AOC_2021_CXX_DAY_ONE_H
+#define AOC_2021_CXX_DAY_ONE_H
+
+#include <cstdlib>
+
+namespace day_one {
+    uint PartOne();
+    uint PartTwo();
+}
+
+#endif //AOC_2021_CXX_DAY_ONE_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "./day_one.h"
+
+int main() {
+    std::cout << "Day One:" << std::endl;
+    std::cout << "  Part One: " << std::to_string(day_one::PartOne()) << std::endl;
+    std::cout << "  Part Two: " << std::to_string(day_one::PartTwo()) << std::endl;
+    return 0;
+}
