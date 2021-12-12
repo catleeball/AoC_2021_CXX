b4b2746a3848f2e3899338ed58a75c35b9283a9e
Cat Lee Ball 🎶🎷🐀 <cat@catball.dev>
2021-12-11 16:03:48 -0800

    WIP: start day two

---
+++ day_two.cpp
@@ -0,0 +1,6 @@
+//
+// Advent of Code Day Two
+//   URL: https://adventofcode.com/2021/day/2
+//
+
+#include "day_two.h"
---
+++ day_two.h
@@ -0,0 +1,18 @@
+//
+// Advent of Code Day Two
+//   URL: https://adventofcode.com/2021/day/2
+//
+
+#ifndef AOC_2021_CXX_DAY_TWO_H
+#define AOC_2021_CXX_DAY_TWO_H
+
+#include <cstdlib>
+
+namespace day_two {
+
+    enum Dir {Up = 'u', Down = 'd', Forward = 'f'};
+
+    uint PartOne();
+}
+
+#endif //AOC_2021_CXX_DAY_TWO_H
